432db8d6cf2cdd71586b92aa164e2e34a1ed8e96
haidang9x <hopthucuahaidang@gmail.com>
2019-08-07 15:12:37 +0700

    fix something
    fix something

--- storage/framework/views/46594ccd13746e8114ef78bd5a25c8fe9ede5318.php
+++ storage/framework/views/46594ccd13746e8114ef78bd5a25c8fe9ede5318.php
@@ -1,9 +1,1 @@
-<?php echo $__env->make('blogs.blocks.header', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-<div id="content">
-    <?php echo $__env->yieldContent('content'); ?>
-</div>
-
-<?php $__env->startSection('scriptDevMod'); ?>
-    ##parent-placeholder-951e41b4b92ca1fa7394c46af4c6595b0d6adb43##
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('blogs.blocks.footer', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+<?php echo $__env->make('blogs.blocks.header', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?> <div id=content> <?php echo $__env->yieldContent('content'); ?> </div> <?php $__env->startSection('scriptDevMod'); ?> ##parent-placeholder-951e41b4b92ca1fa7394c46af4c6595b0d6adb43## <?php $__env->stopSection(); ?> <?php echo $__env->make('blogs.blocks.footer', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
--- storage/framework/views/c526dd4c08f1faaa46bdcaf7230c11e92c1939bb.php
+++ storage/framework/views/c526dd4c08f1faaa46bdcaf7230c11e92c1939bb.php
@@ -1,3 +1,1 @@
-<?php $__currentLoopData = $stores; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $s): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-    <?php echo $__env->make('elements.cat-store-item', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+<?php $__currentLoopData = $stores; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $s): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?> <?php echo $__env->make('elements.cat-store-item', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?> <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
